db1986f3fc6f7f21bdbd0fa14296280c43e69d9c
Matthias <matt.bert@live.fr>
2020-11-18 21:25:02 +0100

    first front configs

--- videogames-front/src/app/app.module.ts
+++ videogames-front/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- videogames-front/src/environments/environment.ts
+++ videogames-front/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backend: {
+    protocol: 'http',
+    host: '0.0.0.0',
+    port: '3000',
+    endpoints: {
+      placeholder: '',
+    }
+  }
 };
 
 /*
